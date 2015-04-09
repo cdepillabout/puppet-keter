4307059d82b9c47acfac7cafa607b0cee5fbcaff
(cdep)illabout <cdep.illabout@gmail.com>
2015-04-09 22:46:46 +0900

    Fix up some problems and get the deb to comile.

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,11 +3,25 @@ class keter::install inherits keter {
 
   case $::osfamily {
     'Debian': {
+
+      $deb_name = "keter_${version}_${architecture}.deb"
+
+      file { '/opt/keter-debs':
+        ensure => directory,
+      }
+
+      file { "/opt/keter-debs/${deb_name}":
+        owner  => 'root',
+        group  => 'root',
+        mode   => '644',
+        ensure => present,
+        source => "puppet:///modules/${module_name}/${deb_name}",
+      }
+
       package { 'keter':
         ensure   => $package_ensure,
         provider => dpkg,
-        source   =>
-          "puppet:///modules/${module_name}/keter_${version}_${architecture}.deb",
+        source   => "/opt/keter-debs/${deb_name}",
       }
     }
     'RedHat': {
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 #
 class keter::params {
 
-  $package_ensure = 'present'
+  $package_ensure = 'latest' #'present'
   $service_enable = true
   $service_ensure = 'running'
   $version        = '1.3.6'
